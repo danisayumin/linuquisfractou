f38903697271719dde91c65d3b97c39260295d7c
Daniela Sayumi <87085378+danisayumin@users.noreply.github.com>
2023-11-16 01:44:55 -0300

    calculo de mandelbrot

--- src/ft_fractol.h
+++ src/ft_fractol.h
@@ -6,7 +6,7 @@
 /*   By: danielasayuminitta <danielasayuminitta@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 21:38:22 by dsayumi-          #+#    #+#             */
-/*   Updated: 2023/11/14 02:03:10 by danielasayu      ###   ########.fr       */
+/*   Updated: 2023/11/15 04:33:50 by danielasayu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,17 @@
 
 #endif
 
-void ft_hook(void* param); //manipula eventos do teclado
-void ft_pixels(mlx_image_t *image);
-uint32_t ft_pixel(uint32_t r, uint32_t g, uint32_t b, uint32_t a);
-int main();
-
-#define GREEN_PIXEL 0xFF00
-
 typedef struct
 {
     double	real;
     double	imag;
 } t_complex;
+
+void ft_hook(void* param); //manipula eventos do teclado
+void ft_pixels(mlx_image_t *image);
+uint32_t ft_pixel(uint32_t r, uint32_t g, uint32_t b, uint32_t a);
+int main();
+int square(t_complex num);
+t_complex multiplica_complexos(t_complex z, t_complex c);
+t_complex soma_complexos(t_complex z, t_complex c);
+int ft_mandelbrot(t_complex c, int max_iterations);
--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -6,20 +6,28 @@
 /*   By: danielasayuminitta <danielasayuminitta@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 20:16:28 by dsayumi-          #+#    #+#             */
-/*   Updated: 2023/11/14 02:19:29 by danielasayu      ###   ########.fr       */
+/*   Updated: 2023/11/16 01:43:46 by danielasayu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_fractol.h"
 
-int ft_mandelbrot(t_complex num)
+int square(t_complex num)
 {
-    //z = z*z + c
 }
 
 t_complex multiplica_complexos(t_complex z, t_complex c)
 {
     t_complex result;
-    result =
+    result.real = z.real * c.real - z.imag * c.imag;
+    result.imag = z.real * c.imag + z.imag * c.real;
+    return result;
+}
+
+t_complex soma_complexos(t_complex z, t_complex c)
+{
+    t_complex result;
+    result.real = z.real + c.real;
+    result.imag = z.imag + c.imag;
     return result;
 }
